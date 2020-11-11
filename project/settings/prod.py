e072bcc704bf7acf8aa60e7b3240c1636e37c4a8
Norma Driske <hello@normade.dev>
2020-11-11 22:42:59 +0100

    Add staticfiles folder

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -13,5 +13,3 @@ ALLOWED_HOSTS = [
 db_from_env = dj_database_url.config(conn_max_age=500)
 
 DATABASES["default"].update(db_from_env)
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
