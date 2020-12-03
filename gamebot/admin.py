e55f1f59b4fa866e6359e041959c2ffeb3ee977d
Norma Driske <hello@normade.dev>
2020-12-03 21:27:11 +0100

    Add communication category

--- gamebot/admin.py
+++ gamebot/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from gamebot.models import CommunicationCategory
 
-# Register your models here.
+
+@admin.register(CommunicationCategory)
+class CommunicationCategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ["id",]
--- gamebot/models.py
+++ gamebot/models.py
@@ -1,3 +1,14 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class CommunicationCategory(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    name = models.CharField(max_length=32)
+    accepted_keywords = models.TextField(blank=True)
+    bot_answer = models.TextField(blank=True)
+    link_url = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
